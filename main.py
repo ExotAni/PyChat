7859fc16d10dd6645de7aea4078efc42a7ac5a3e
ExotAni <54.hadjiev@mail.ru>
2024-06-03 13:49:46 +0300

    Тестовый одиночный чат

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ DIRECTORY = r"D:\1337\things\PyChat"
 
 def main():
     while True:
+        cmd("title Menu")
         cmd("cls")
         
         print(f"Welcome to the club, {name}\n")
@@ -18,28 +19,33 @@ def main():
         choice = input()
         if choice == "1": chat()
         elif choice == "2":
+            cmd("title Settings")
             cmd("cls")
             print("Coming soon")
-            break
+            cmd("pause")
         elif choice == "3": break
             
 
 def chat():
+    cmd("title PyChat")
     cmd("cls")
-    cmd('if not exist '+ DIRECTORY + '\\history.json echo {"messages":[]}>' + DIRECTORY + "\\history.json")
+    cmd('if not exist '+ DIRECTORY + '\\history.json echo {"messages":[{"time": 0,"name":"System","message":"[%date%]"}]}>' + DIRECTORY + "\\history.json")
     while True:
         cmd("cls")
         print("PyChat starting here!\n")
         temp_history = json.load(open(fr"{DIRECTORY}\history.json"))
-        # print(temp_history.get("messages")[-1])
         for message in temp_history.get("messages"):
-            print(f"[{datetime.fromtimestamp(float(message.get("time"))).strftime("%H:%M:%S")}] {message.get("name")}: {message.get("message")}")
+            if message.get("name") == "System":
+                print(f"{message.get("message")}")
+            else:
+                print(f"[{datetime.fromtimestamp(float(message.get("time"))).strftime("%H:%M:%S")}] {message.get("name")}: {message.get("message")}")
         print()
         message = input(">>>")
         if message == "/exit": break
+        if temp_history.get("messages")[-1].get("time") != 0 and datetime.fromtimestamp(float(datetime.now().timestamp())).strftime("[%d.%m.%Y]") != datetime.fromtimestamp(float(temp_history.get("messages")[-1].get("time"))).strftime("[%d.%m.%Y]"):
+            temp_history["messages"].append({"time": datetime.now().timestamp(), "name": "System", "message": datetime.fromtimestamp(float(datetime.now().timestamp())).strftime("[%d.%m.%Y]")})
         temp_history["messages"].append({"time": datetime.now().timestamp(), "name": name, "message": message})
         json.dump(temp_history, open(fr"{DIRECTORY}\history.json", "w"))
         
 
 main()
-cmd("pause")
